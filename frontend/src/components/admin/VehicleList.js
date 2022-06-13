7b770b29ae78863f01049db0cbcae3962e408967
faizan-devv <faizanrizwan2277@gmail.com>
2022-06-14 01:27:54 +0500

    vehicle functionality

--- frontend/src/components/admin/VehicleList.js
+++ frontend/src/components/admin/VehicleList.js
@@ -1,63 +1,113 @@
-import React, {Fragment} from 'react'
-import MetaData from '../layout/MetaData'
-import Sidebar from './Sidebar'
-import { Link } from 'react-router-dom'
+import React, { Fragment, useEffect, useState } from "react";
+import MetaData from "../layout/MetaData";
+import Sidebar from "./Sidebar";
+import { Link } from "react-router-dom";
+import axios from "axios";
+
 const VehicleList = () => {
-    return (
-        <Fragment>
-        <MetaData title={'Vehicle List'} />
-        <div className="row">
-            <div className="col-12 col-md-2">
-                <Sidebar />
-            </div>
-    
-            <div className="col-12 col-md-10" >
-                <Fragment>
-                <h1 className="my-5">All Vehicle</h1>
-                <div id="example_wrapper" className="dataTables_wrapper my-5"><div className="dataTables_length" id="example_length"><label>Show <select name="example_length" aria-controls="example" className><option value={10}>10</option><option value={25}>25</option><option value={50}>50</option><option value={100}>100</option></select> entries</label></div><div id="example_filter" className="dataTables_filter"><label>Search:<input type="search" className placeholder aria-controls="example" /></label></div><table id="example" className="display dataTable" style={{width: '100%'}} aria-describedby="example_info">
-        
-    
-    
-                <table class="table">
-         <thead>
-             <tr>
-              <th>Vehicle ID</th>
-              <th>Vehicle Name</th>
-              <th>Owner</th>
-              <th>Engine No</th>
-              <th>Action</th>
-             </tr>
-         </thead>
-         <tbody>
-               <tr>
-                     <td data-label="S.No">1</td>
-                     <td data-label="Name">truck tractor trailer</td>
-                     <td data-label="Age">Arshad</td>
-                     <td data-label="Marks%">mrg32k3a</td>
-                 <td data-label="Staus"> <Link to="/admin/updatevehicle"><i className="fa fa-pencil"></i> </Link>  <i className="fa fa-trash"></i> </td>
-               </tr>
-    
-               <tr>
-                     <td data-label="S.No">2</td>
-                     <td data-label="Name">Cat Mid wheel Loader</td>
-                     <td data-label="Age">Iqbal</td>
-                     <td data-label="Marks%">philox4x32x10</td>
-                     <td data-label="Staus"> <Link to="/admin/updatevehicle"><i className="fa fa-pencil"></i> </Link>  <i className="fa fa-trash"></i></td>
-               </tr>
-    
-              
-         </tbody>
-       </table>
-      </table><div className="dataTables_info" id="example_info" role="status" aria-live="polite">Showing 1 to 10 of 57 entries</div>
-      </div>
-    
-                </Fragment>
-            </div>
+  const [vehicleData, setVehicleData] = useState([]);
+
+  useEffect(() => {
+    axios
+      .get("http://localhost:4000/api/v1/admin/vehicles")
+      .then((response) => {
+        if (response.data.success) setVehicleData(response.data.vehicles);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  }, []);
+
+  return (
+    <Fragment>
+      <MetaData title={"Vehicle List"} />
+      <div className="row">
+        <div className="col-12 col-md-2">
+          <Sidebar />
         </div>
-    
+
+        <div className="col-12 col-md-10">
+          <Fragment>
+            <h1 className="my-5">All Vehicle</h1>
+            <div id="example_wrapper" className="dataTables_wrapper my-5">
+              <div className="dataTables_length" id="example_length">
+                <label>
+                  Show{" "}
+                  <select
+                    name="example_length"
+                    aria-controls="example"
+                    className
+                  >
+                    <option value={10}>10</option>
+                    <option value={25}>25</option>
+                    <option value={50}>50</option>
+                    <option value={100}>100</option>
+                  </select>{" "}
+                  entries
+                </label>
+              </div>
+              <div id="example_filter" className="dataTables_filter">
+                <label>
+                  Search:
+                  <input
+                    type="search"
+                    className
+                    placeholder
+                    aria-controls="example"
+                  />
+                </label>
+              </div>
+              <table
+                id="example"
+                className="display dataTable"
+                style={{ width: "100%" }}
+                aria-describedby="example_info"
+              >
+                <table class="table">
+                  <thead>
+                    <tr>
+                      <th>Vehicle ID</th>
+                      <th>Vehicle Name</th>
+                      <th>Owner</th>
+                      <th>Engine No</th>
+                      <th>Action</th>
+                    </tr>
+                  </thead>
+                  <tbody>
+                    {vehicleData?.map((vehicle) => {
+                      return (
+                        <tr key={vehicle._id}>
+                          <td data-label="S.No">{vehicle._id}</td>
+                          <td data-label="Name">{vehicle.name}</td>
+                          <td data-label="Age">{vehicle.ownerName}</td>
+                          <td data-label="Marks%">{vehicle.engineNo}</td>
+                          <td data-label="Staus">
+                            {" "}
+                            <Link to={`/admin/updatevehicle/${vehicle._id}`}>
+                              <i className="fa fa-pencil"></i>{" "}
+                            </Link>{" "}
+                            <i className="fa fa-trash"></i>{" "}
+                          </td>
+                        </tr>
+                      );
+                    })}
+                  </tbody>
+                </table>
+              </table>
+              <div
+                className="dataTables_info"
+                id="example_info"
+                role="status"
+                aria-live="polite"
+              >
+                Showing 1 to 10 of 57 entries
+              </div>
+            </div>
+          </Fragment>
+        </div>
+      </div>
     </Fragment>
-    )
-}
+  );
+};
 
-
-export default VehicleList
+export default VehicleList;
--- frontend/src/components/product/VehicleShow.js
+++ frontend/src/components/product/VehicleShow.js
@@ -1,147 +1,64 @@
-import React from 'react'
-import { Link } from 'react-router-dom'
-
-
+import React, { useEffect, useState } from "react";
+import { Link } from "react-router-dom";
+import axios from "axios";
 
 const VehicleShow = () => {
+  const [vehicleData, setVehicleData] = useState([]);
+  useEffect(() => {
+    axios
+      .get(`http://localhost:4000/api/v1/vehicles`)
+      .then((response) => {
+        if (response.data.success) setVehicleData(response.data.vehiles);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  }, []);
   return (
     <>
-    
-    <h1 id="products_heading" className="my-5">Vehicle</h1>
-<section className="section service-2">
-<div className="container">
-<div className="row">
-  <div className="col-lg-4 col-md-6 col-sm-6">
-    <div className="service-block mb-5">
-      <img src="images/1vehicle.jpg" alt className="img-fluid" />
-      <div className="card-body d-flex flex-column">
-      <h5 className="card-title">
-        <Link to="">Crane</Link>
-      </h5>
-      <div className="ratings mt-auto">
-        <div className="rating-outer">
-          <div className="rating-inner"></div>
+      <h1 id="products_heading" className="my-5">
+        Vehicle
+      </h1>
+      <section className="section service-2">
+        <div className="container">
+          <div className="row">
+            {vehicleData?.map((vehicle) => {
+              return (
+                <div className="col-lg-4 col-md-6 col-sm-6">
+                  <div className="service-block mb-5">
+                    <img
+                      src={vehicle.images[0].url}
+                      alt
+                      className="img-fluid"
+                    />
+                    <div className="card-body d-flex flex-column">
+                      <h5 className="card-title">
+                        <Link to="">{vehicle.name}</Link>
+                      </h5>
+                      <div className="ratings mt-auto">
+                        <div className="rating-outer">
+                          <div className="rating-inner"></div>
+                        </div>
+                        <span id="no_of_reviews">(5 Reviews)</span>
+                      </div>
+                      <p className="card-text">Rs 1300.67 per hours</p>
+                      <Link
+                        to="/productdetails"
+                        id="view_btn"
+                        className="btn btn-block "
+                      >
+                        View Details
+                      </Link>
+                    </div>
+                  </div>
+                </div>
+              );
+            })}
+          </div>
         </div>
-        <span id="no_of_reviews">(5 Reviews)</span>
-      </div>
-      <p className="card-text">Rs 1300.67 per hours</p>
-      <Link to="/productdetails" id="view_btn" className="btn btn-block ">View Details</Link>
-    </div>
-    </div>
-  </div>
-  <div className="col-lg-4 col-md-6 col-sm-6">
-    <div className="service-block mb-5">
-      <img src="images/2vehicle.jpg" alt className="img-fluid" />
-      <div className="card-body d-flex flex-column">
-      <h5 className="card-title">
-      
-      <Link href=""
-          >truck</Link>
-      </h5>
-      <div className="ratings mt-auto">
-       <i className="fa fa-star"></i>
-        <i className="fa fa-star"></i>
-        <i className="fa fa-star"></i>
-        <i className="fa fa-star-half-o"></i>
-        <i className="fa fa-star-o"></i>
-        <span id="no_of_reviews">(15 Reviews)</span>
-      </div>
-      <p className="card-text">Rs 965 per hours</p>
-      <a href="#" id="view_btn" className="btn btn-block">View Details</a>
-    </div>
-    </div>
-  </div>
-  <div className="col-lg-4 col-md-6 col-sm-6">
-    <div className="service-block mb-5">
-      <img src="images/3vehicle.jpg" alt className="img-fluid" />
-      <div className="card-body d-flex flex-column">
-      <h5 className="card-title">
-        <Link href=""
-          >water tank</Link>
-      </h5>
-      <div className="ratings mt-auto">
-        <i className="fa fa-star"></i>
-        <i className="fa fa-star"></i>
-        <i className="fa fa-star"></i>
-        <i className="fa fa-star-half-o"></i>
-        <i className="fa fa-star-o"></i>
-        <span id="no_of_reviews">(5 Reviews)</span>
-      </div>
-      <p className="card-text">Rs 725.57 per hours</p>
-      <Link href="#" id="view_btn" className="btn btn-block">View Details</Link>
-    </div>
-    </div>
-  </div>
-  <div className="col-lg-4 col-md-6 col-sm-6">
-    <div className="service-block mb-5 mb-lg-0">
-      <img src="images/2vehicle.jpg" alt className="img-fluid" />
-      <div className="card-body d-flex flex-column">
-      <h5 className="card-title">
-        <Link href="">truck</Link>
-      </h5>
-      <div className="ratings mt-auto">
-       <i className="fa fa-star"></i>
-        <i className="fa fa-star"></i>
-        <i className="fa fa-star"></i>
-        <i className="fa fa-star-half-o"></i>
-        <i className="fa fa-star-o"></i>
-        <span id="no_of_reviews">(5 Reviews)</span>
-      </div>
-      <p className="card-text">Rs 975.56 per hours</p>
+      </section>
+    </>
+  );
+};
 
-      <Link type="button" href="#" id="view_btn" className="btn btn-block">View Details</Link>
-    </div>
-    </div>
-  </div>
-  <div className="col-lg-4 col-md-6 col-sm-6">
-    <div className="service-block mb-5 mb-lg-0">
-      <img src="images/3vehicle.jpg" alt className="img-fluid" />
-      <div className="card-body d-flex flex-column">
-      <h5 className="card-title">
-        <Link href="">water tank</Link>
-      </h5>
-      <div className="ratings mt-auto">
-        <i className="fa fa-star"></i>
-        <i className="fa fa-star"></i>
-        <i className="fa fa-star"></i>
-        <i className="fa fa-star-half-o"></i>
-        <i className="fa fa-star-o"></i>
-        <span id="no_of_reviews">(5 Reviews)</span>
-      </div>
-      <p className="card-text">RS 75.56 per hours</p>
-      <Link href="#" id="view_btn" className="btn btn-block">View Details</Link>
-    </div>
-    </div>
-  </div>
-  <div className="col-lg-4 col-md-6 col-sm-6">
-    <div className="service-block mb-5 mb-lg-0">
-      <img src="images/1vehicle.jpg" alt className="img-fluid" />
-      <div className="card-body d-flex flex-column">
-      <h5 className="card-title">
-        <Link href="">crane</Link>
-      </h5>
-      <div className="ratings mt-auto">
-        <i className="fa fa-star"></i>
-        <i className="fa fa-star"></i>
-        <i className="fa fa-star"></i>
-        <i className="fa fa-star-half-o"></i>
-        <i className="fa fa-star-o"></i>
-        <span id="no_of_reviews">(5 Reviews)</span>
-      </div>
-      <p className="card-text">RS 75.56 per hours</p>
-      <Link href="#" id="view_btn" className="btn btn-block">View Details</Link>
-    </div>
-    </div>
-  </div>
-</div>
-</div>
-</section>
-
-   
-   </> 
-   
-   
-)
-}
-
-export default VehicleShow
+export default VehicleShow;
